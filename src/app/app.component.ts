569503edd5a4b936e81d95b0ca547514fae20cb2
Danilo Herdy Oliveira <daniloho89@gmail.com>
2020-01-06 20:27:45 -0300

    Aula 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+  description = 'Dragão';
+  url = 'https://symbolismofthings.com/wp-content/uploads/2019/03/simbolismo-do-dragao-e1553022703285.jpg';
 }
