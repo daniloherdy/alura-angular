4c5d716b9a52999a5e5927a8d1ea9b5907e42272
Danilo Herdy Oliveira <daniloho89@gmail.com>
2020-02-09 15:20:21 -0300

    Angular 1 - Aula 05

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
+import { Photo } from '../photo/photo';
 
 @Component({
   selector: 'app-photo-list',
@@ -9,7 +10,7 @@ import { PhotoService } from '../photo/photo.service';
 })
 export class PhotoListComponent implements OnInit {
 
-  photos: any[] = [];
+  photos: Photo[] = [];
 
   constructor(
     private photoService: PhotoService,
