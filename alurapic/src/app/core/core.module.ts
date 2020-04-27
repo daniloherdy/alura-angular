57a1eb7e43540f08802bc3199fd65863c40b727b
Danilo Herdy Oliveira <daniloho89@gmail.com>
2020-04-27 06:26:38 -0300

    Parte 2 - Aula 05
    rotas filhas;
    como buildar o projeto;
    Lazy loading e code splitting;
    interceptadores e envio de token

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptor } from './auth/request.interceptor';
 
 @NgModule({
 
@@ -10,6 +12,13 @@ import { RouterModule } from '@angular/router';
         CommonModule,
         RouterModule],
     declarations: [HeaderComponent],
-    exports: [HeaderComponent]
+    exports: [HeaderComponent],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: RequestInterceptor,
+            multi: true
+        }
+    ]
 })
 export class CoreModule { }
